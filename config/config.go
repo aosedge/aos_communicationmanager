34db25efff4247280abc545720c5596004643293
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2021-09-01 16:16:37 +0300

    [config] Move CM related config from SM
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ config/config.go
@@ -0,0 +1,222 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright 2021 Renesas Inc.
+// Copyright 2021 EPAM Systems Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package config provides set of API to provide aos configuration
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"path"
+	"time"
+
+	"gitpct.epam.com/epmd-aepr/aos_common/aoserrors"
+)
+
+/***********************************************************************************************************************
+ * Types
+ **********************************************************************************************************************/
+
+// Crypt configuration structure with crypto attributes
+type Crypt struct {
+	CACert        string `json:"CACert"`
+	TpmDevice     string `json:"tpmDevice,omitempty"`
+	Pkcs11Library string `json:"pkcs11Library,omitempty"`
+}
+
+// UMController configuration for update controller
+type UMController struct {
+	ServerURL     string           `json:"serverUrl"`
+	FileServerURL string           `json:"fileServerUrl,omitempty"`
+	UMClients     []UMClientConfig `json:"umClients"`
+	UpdateDir     string           `json:"updateDir"`
+}
+
+// UMClientConfig update manager config
+type UMClientConfig struct {
+	UMID     string `json:"umId"`
+	Priority uint32 `json:"priority"`
+	IsLocal  bool   `json:"isLocal,omitempty"`
+}
+
+// Duration represents duration in format "00:00:00"
+type Duration struct {
+	time.Duration
+}
+
+// AlertRule describes alert rule
+type AlertRule struct {
+	MinTimeout   Duration `json:"minTimeout"`
+	MinThreshold uint64   `json:"minThreshold"`
+	MaxThreshold uint64   `json:"maxThreshold"`
+}
+
+// Monitoring configuration for system monitoring
+type Monitoring struct {
+	MaxOfflineMessages int        `json:"maxOfflineMessages"`
+	SendPeriod         Duration   `json:"sendPeriod"`
+	PollPeriod         Duration   `json:"pollPeriod"`
+	RAM                *AlertRule `json:"ram"`
+	CPU                *AlertRule `json:"cpu"`
+	UsedDisk           *AlertRule `json:"usedDisk"`
+	InTraffic          *AlertRule `json:"inTraffic"`
+	OutTraffic         *AlertRule `json:"outTraffic"`
+}
+
+// Logging configuration for system and service logging
+type Logging struct {
+	MaxPartSize  uint64 `json:"maxPartSize"`
+	MaxPartCount uint64 `json:"maxPartCount"`
+}
+
+// Alerts configuration for alerts
+type Alerts struct {
+	Disabled           bool     `json:"disabled"`
+	SendPeriod         Duration `json:"sendPeriod"`
+	MaxMessageSize     int      `json:"maxMessagesize"`
+	MaxOfflineMessages int      `json:"maxOfflineMessages"`
+	Filter             []string `json:"filter"`
+}
+
+// Migration struct represents path for db migration
+type Migration struct {
+	MigrationPath       string `json:"migrationPath"`
+	MergedMigrationPath string `json:"mergedMigrationPath"`
+}
+
+// Config instance
+type Config struct {
+	Crypt                 Crypt        `json:"fcrypt"`
+	CertStorage           string       `json:"certStorage"`
+	ServiceDiscoveryURL   string       `json:"serviceDiscoveryUrl"`
+	IAMServerURL          string       `json:"iamServerUrl"`
+	WorkingDir            string       `json:"workingDir"`
+	DownloadDir           string       `json:"downloadDir"`
+	DownloadFileTTLDays   uint64       `json:"downloadFileTTLDays"`
+	BoardConfigFile       string       `json:"boardConfigFile"`
+	UnitStatusSendTimeout Duration     `json:"unitStatusSendTimeout"`
+	Monitoring            Monitoring   `json:"monitoring"`
+	Logging               Logging      `json:"logging"`
+	Alerts                Alerts       `json:"alerts"`
+	Migration             Migration    `json:"migration"`
+	UMController          UMController `json:"umController"`
+}
+
+/***********************************************************************************************************************
+ * Public
+ **********************************************************************************************************************/
+
+// New creates new config object
+func New(fileName string) (config *Config, err error) {
+	raw, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return config, err
+	}
+
+	config = &Config{
+		UnitStatusSendTimeout: Duration{30 * time.Second},
+		DownloadFileTTLDays:   3,
+		Monitoring: Monitoring{
+			SendPeriod:         Duration{1 * time.Minute},
+			PollPeriod:         Duration{10 * time.Second},
+			MaxOfflineMessages: 25},
+		Logging: Logging{
+			MaxPartSize:  524288,
+			MaxPartCount: 20},
+		Alerts: Alerts{
+			SendPeriod:         Duration{10 * time.Second},
+			MaxMessageSize:     65536,
+			MaxOfflineMessages: 25}}
+
+	if err = json.Unmarshal(raw, &config); err != nil {
+		return config, aoserrors.Wrap(err)
+	}
+
+	if config.CertStorage == "" {
+		config.CertStorage = "/var/aos/crypt/cm/"
+	}
+
+	if config.DownloadDir == "" {
+		config.DownloadDir = path.Join(config.WorkingDir, "download")
+	}
+
+	if config.BoardConfigFile == "" {
+		config.BoardConfigFile = path.Join(config.WorkingDir, "aos_board.cfg")
+	}
+
+	if config.Migration.MigrationPath == "" {
+		config.Migration.MigrationPath = "/usr/share/aos/communicationmanager/migration"
+	}
+
+	if config.Migration.MergedMigrationPath == "" {
+		config.Migration.MergedMigrationPath = path.Join(config.WorkingDir, "migration")
+	}
+
+	if config.UMController.UpdateDir == "" {
+		config.UMController.UpdateDir = path.Join(config.WorkingDir, "update")
+	}
+
+	return config, nil
+}
+
+// MarshalJSON marshals JSON Duration type
+func (d Duration) MarshalJSON() (b []byte, err error) {
+	t, err := time.Parse("15:04:05", "00:00:00")
+	if err != nil {
+		return nil, aoserrors.Wrap(err)
+	}
+	t.Add(d.Duration)
+
+	return json.Marshal(t.Add(d.Duration).Format("15:04:05"))
+}
+
+// UnmarshalJSON unmarshals JSON Duration type
+func (d *Duration) UnmarshalJSON(b []byte) (err error) {
+	var v interface{}
+
+	if err := json.Unmarshal(b, &v); err != nil {
+		return aoserrors.Wrap(err)
+	}
+
+	switch value := v.(type) {
+	case float64:
+		d.Duration = time.Duration(value) * time.Second
+		return nil
+
+	case string:
+		tmp, err := time.ParseDuration(value)
+		if err != nil {
+			t1, err := time.Parse("15:04:05", value)
+			if err != nil {
+				return aoserrors.Wrap(err)
+			}
+			t2, err := time.Parse("15:04:05", "00:00:00")
+			if err != nil {
+				return aoserrors.Wrap(err)
+			}
+
+			tmp = t1.Sub(t2)
+		}
+
+		d.Duration = tmp
+
+		return nil
+
+	default:
+		return aoserrors.Errorf("invalid duration value: %v", value)
+	}
+}
---
+++ config/config_test.go
@@ -0,0 +1,283 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright 2021 Renesas Inc.
+// Copyright 2021 EPAM Systems Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config_test
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"reflect"
+	"testing"
+	"time"
+
+	"aos_communicationmanager/config"
+)
+
+/***********************************************************************************************************************
+ * Consts
+ **********************************************************************************************************************/
+
+const testConfigContent = `{
+	"fcrypt" : {
+		"CACert" : "CACert",
+		"tpmDevice": "/dev/tpmrm0",
+		"pkcs11Library": "/path/to/pkcs11/library"
+	},
+	"certStorage": "/var/aos/crypt/cm/",
+	"serviceDiscoveryUrl" : "www.aos.com",
+	"iamServerUrl" : "localhost:8090",
+	"workingDir" : "workingDir",
+	"boardConfigFile" : "/var/aos/aos_board.cfg",
+	"monitoring": {
+		"sendPeriod": "00:05:00",
+		"pollPeriod": "00:00:01",
+		"maxOfflineMessages": 25,
+		"ram": {
+			"minTimeout": "00:00:10",
+			"minThreshold": 10,
+			"maxThreshold": 150
+		},
+		"outTraffic": {
+			"minTimeout": "00:00:20",
+			"minThreshold": 10,
+			"maxThreshold": 150
+		}
+	},
+	"logging": {
+		"maxPartSize": 1024,
+		"maxPartCount": 10
+	},
+	"alerts": {
+		"sendPeriod": "00:00:20",
+		"maxMessageSize": 1024,
+		"maxOfflineMessages": 32,
+		"filter": ["(test)", "(regexp)"]
+	},
+	"migration": {
+		"migrationPath" : "/usr/share/aos_communicationmanager/migration",
+		"mergedMigrationPath" : "/var/aos/communicationmanager/migration"
+	},
+	"umController": {
+		"serverUrl": "localhost:8091",
+		"fileServerUrl": "localhost:8092",
+		"umClients": [{
+			"umId": "um",
+			"priority": 0,
+			"isLocal": true
+		}],
+		"updateDir": "/var/aos/update"
+	}
+}`
+
+/***********************************************************************************************************************
+ * Vars
+ **********************************************************************************************************************/
+
+var tmpDir string
+var testCfg *config.Config
+
+/***********************************************************************************************************************
+ * Main
+ **********************************************************************************************************************/
+
+func TestMain(m *testing.M) {
+	if err := setup(); err != nil {
+		log.Fatalf("Error creating service images: %s", err)
+	}
+
+	ret := m.Run()
+
+	if err := cleanup(); err != nil {
+		log.Fatalf("Error cleaning up: %s", err)
+	}
+
+	os.Exit(ret)
+}
+
+/***********************************************************************************************************************
+ * Tests
+ **********************************************************************************************************************/
+
+func TestGetCrypt(t *testing.T) {
+	if testCfg.Crypt.TpmDevice != "/dev/tpmrm0" {
+		t.Errorf("Wrong TPMEngine Interface value: %s", testCfg.Crypt.TpmDevice)
+	}
+
+	if testCfg.Crypt.CACert != "CACert" {
+		t.Errorf("Wrong CACert value: %s", testCfg.Crypt.CACert)
+	}
+
+	if testCfg.Crypt.Pkcs11Library != "/path/to/pkcs11/library" {
+		t.Errorf("Wrong PKCS11 library value: %s", testCfg.Crypt.Pkcs11Library)
+	}
+}
+
+func TestGetServiceDiscoveryURL(t *testing.T) {
+	if testCfg.ServiceDiscoveryURL != "www.aos.com" {
+		t.Errorf("Wrong server URL value: %s", testCfg.ServiceDiscoveryURL)
+	}
+}
+
+func TestGetWorkingDir(t *testing.T) {
+	if testCfg.WorkingDir != "workingDir" {
+		t.Errorf("Wrong working directory value: %s", testCfg.WorkingDir)
+	}
+}
+
+func TestGetBoardConfigFile(t *testing.T) {
+	if testCfg.BoardConfigFile != "/var/aos/aos_board.cfg" {
+		t.Errorf("Wrong board config file value: %s", testCfg.BoardConfigFile)
+	}
+}
+
+func TestGetIAMServerURL(t *testing.T) {
+	if testCfg.IAMServerURL != "localhost:8090" {
+		t.Errorf("Wrong IAM server value: %s", testCfg.IAMServerURL)
+	}
+}
+
+func TestDurationMarshal(t *testing.T) {
+	d := config.Duration{Duration: 32 * time.Second}
+
+	result, err := json.Marshal(d)
+	if err != nil {
+		t.Errorf("Can't marshal: %s", err)
+	}
+
+	if string(result) != `"00:00:32"` {
+		t.Errorf("Wrong value: %s", result)
+	}
+}
+
+func TestGetMonitoringConfig(t *testing.T) {
+	if testCfg.Monitoring.SendPeriod.Duration != 5*time.Minute {
+		t.Errorf("Wrong send period value: %s", testCfg.Monitoring.SendPeriod)
+	}
+
+	if testCfg.Monitoring.PollPeriod.Duration != 1*time.Second {
+		t.Errorf("Wrong poll period value: %s", testCfg.Monitoring.PollPeriod)
+	}
+
+	if testCfg.Monitoring.RAM.MinTimeout.Duration != 10*time.Second {
+		t.Errorf("Wrong value: %s", testCfg.Monitoring.RAM.MinTimeout)
+	}
+
+	if testCfg.Monitoring.OutTraffic.MinTimeout.Duration != 20*time.Second {
+		t.Errorf("Wrong value: %s", testCfg.Monitoring.RAM.MinTimeout)
+	}
+}
+
+func TestGetLoggingConfig(t *testing.T) {
+	if testCfg.Logging.MaxPartSize != 1024 {
+		t.Errorf("Wrong max part size: %d", testCfg.Logging.MaxPartSize)
+	}
+
+	if testCfg.Logging.MaxPartCount != 10 {
+		t.Errorf("Wrong max part count: %d", testCfg.Logging.MaxPartCount)
+	}
+}
+
+func TestGetAlertsConfig(t *testing.T) {
+	if testCfg.Alerts.SendPeriod.Duration != 20*time.Second {
+		t.Errorf("Wrong poll period value: %s", testCfg.Alerts.SendPeriod)
+	}
+
+	if testCfg.Alerts.MaxMessageSize != 1024 {
+		t.Errorf("Wrong max message size value: %d", testCfg.Alerts.MaxMessageSize)
+	}
+
+	if testCfg.Alerts.MaxOfflineMessages != 32 {
+		t.Errorf("Wrong max offline message value: %d", testCfg.Alerts.MaxOfflineMessages)
+	}
+
+	filter := []string{"(test)", "(regexp)"}
+
+	if !reflect.DeepEqual(testCfg.Alerts.Filter, filter) {
+		t.Errorf("Wrong filter value: %v", testCfg.Alerts.Filter)
+	}
+}
+
+func TestUMControllerConfig(t *testing.T) {
+	umClient := config.UMClientConfig{UMID: "um", Priority: 0, IsLocal: true}
+
+	originalConfig := config.UMController{ServerURL: "localhost:8091",
+		FileServerURL: "localhost:8092",
+		UMClients:     []config.UMClientConfig{umClient},
+		UpdateDir:     "/var/aos/update",
+	}
+
+	if !reflect.DeepEqual(originalConfig, testCfg.UMController) {
+		t.Errorf("Wrong UM controller value: %v", testCfg.UMController)
+	}
+}
+
+func TestDatabaseMigration(t *testing.T) {
+	if testCfg.Migration.MigrationPath != "/usr/share/aos_communicationmanager/migration" {
+		t.Errorf("Wrong migration path value: %s", testCfg.Migration.MigrationPath)
+	}
+
+	if testCfg.Migration.MergedMigrationPath != "/var/aos/communicationmanager/migration" {
+		t.Errorf("Wrong merged migration path value: %s", testCfg.Migration.MergedMigrationPath)
+	}
+}
+
+func TestCertStorage(t *testing.T) {
+	if testCfg.CertStorage != "/var/aos/crypt/cm/" {
+		t.Errorf("Wrong certificate storage value: %s", testCfg.CertStorage)
+	}
+}
+
+/***********************************************************************************************************************
+ * Private
+ **********************************************************************************************************************/
+
+func createConfigFile(fileName string) (err error) {
+	if err := ioutil.WriteFile(fileName, []byte(testConfigContent), 0644); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func setup() (err error) {
+	if tmpDir, err = ioutil.TempDir("", "aos_"); err != nil {
+		return err
+	}
+
+	fileName := path.Join(tmpDir, "aos_communicationmanager.cfg")
+
+	if err = createConfigFile(fileName); err != nil {
+		return err
+	}
+
+	if testCfg, err = config.New(fileName); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func cleanup() (err error) {
+	if err := os.RemoveAll(tmpDir); err != nil {
+		return err
+	}
+
+	return nil
+}
