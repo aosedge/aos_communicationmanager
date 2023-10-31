1bb5d2eed2d0ca7546a56771fdc1a0bf69b85b60
Mykola Solianko <mykola_solianko@epam.com>
2023-10-31 17:06:35 +0200

    [networkmanager] Remove allocated network from cache
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>

--- networkmanager/ipamalloc.go
+++ networkmanager/ipamalloc.go
@@ -172,6 +172,53 @@ func (ipam *ipSubnet) getAvailableSubnet(networkID string) (*net.IPNet, error) {
 	return subnet.ipNet, nil
 }
 
+func (ipam *ipSubnet) removeAllocatedSubnets(networks []NetworkInfo, networkInstances []InstanceNetworkInfo) {
+	ipam.Lock()
+	defer ipam.Unlock()
+
+	for _, network := range networks {
+		_, ipNet, err := net.ParseCIDR(network.Subnet)
+		if err != nil {
+			log.Errorf("Failed to parse subnet %s: %v", network.Subnet, err)
+
+			continue
+		}
+
+		for i, ipNetPool := range ipam.predefinedPrivateNetworks {
+			if ipNetPool.String() == ipNet.String() {
+				ipam.usedIPSubnets[network.NetworkID] = subnetwork{
+					ipNet: ipNetPool,
+					ips:   generateSubnetIPs(ipNetPool),
+				}
+
+				ipam.predefinedPrivateNetworks = append(
+					ipam.predefinedPrivateNetworks[:i], ipam.predefinedPrivateNetworks[i+1:]...)
+
+				log.Debugf("Allocated subnet %s was removed", ipNet.String())
+
+				break
+			}
+		}
+	}
+
+	for _, networkInstance := range networkInstances {
+		subnet, ok := ipam.usedIPSubnets[networkInstance.NetworkID]
+		if !ok {
+			continue
+		}
+
+		for i, ip := range subnet.ips {
+			if ip.String() == networkInstance.IP {
+				subnet.ips = append(subnet.ips[:i], subnet.ips[i+1:]...)
+
+				log.Debugf("Allocated ip %s was removed", ip.String())
+
+				break
+			}
+		}
+	}
+}
+
 func generateSubnetIPs(ipNet *net.IPNet) []net.IP {
 	var (
 		minIPRange, _ = cidr.AddressRange(ipNet)
