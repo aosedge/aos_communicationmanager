6efdeca799d576f1ae9e6e928ea9a0b311de1e8d
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2024-10-09 14:42:56 +0300

    [unitstatushandler] Fix wrong return from handleChannels function
    On receiving fall resource alert status we should continue to serve channels.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- unitstatushandler/unitstatushandler.go
+++ unitstatushandler/unitstatushandler.go
@@ -792,7 +792,7 @@ func (instance *Instance) handleChannels() {
 			}
 
 			if systemQuotaAlert.Status == resourcemonitor.AlertStatusFall {
-				return
+				continue
 			}
 
 			if slices.Contains([]string{"cpu", "ram"}, systemQuotaAlert.Parameter) {
